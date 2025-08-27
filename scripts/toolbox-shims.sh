cf1ff9db8316b4fe673aad968367aeeef81467e3
Cory Farmer <mr@cory.farm>
2025-08-27 19:58:02 -0400

    fix host-spawn, add fedora packages

--- scripts/toolbox-shims.sh
+++ scripts/toolbox-shims.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-curl "https://github.com/1player/host-spawn/releases/latest/download/host-spawn-$(uname -m)" -so /usr/local/bin/host-spawn
+curl "https://github.com/1player/host-spawn/releases/latest/download/host-spawn-$(uname -m)" -fsLSo /usr/local/bin/host-spawn
 chmod +x /usr/local/bin/host-spawn
 
 [ ! -e /usr/bin/sh ] && ln -fs /bin/sh /usr/bin/sh
