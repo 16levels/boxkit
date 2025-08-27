dc9b0da06176729c9ef3be89b9c3cc2e8a68c715
Cory Farmer <mr@cory.farm>
2025-08-27 17:09:06 -0400

    add starship and lazygit copr

--- scripts/fedora.sh
+++ scripts/fedora.sh
@@ -4,5 +4,7 @@
 ./distrobox-shims.sh
 
 # Update the container and install packages
+dnf copr enable atim/lazygit -y
+dnf copr enable atim/starship -y
 dnf update -y
 grep -v '^#' ./fedora.packages | xargs dnf install -y
