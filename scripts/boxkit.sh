844f284256f77cb6bcc7fecd98868c658ef71450
Cory Farmer <mr@cory.farm>
2025-08-27 15:32:35 -0400

    remove boxkit.sh

--- scripts/boxkit.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Symlink distrobox shims
-./distrobox-shims.sh
-
-# Update the container and install packages
-apk update && apk upgrade
-grep -v '^#' ./alpine.packages | xargs apk add
