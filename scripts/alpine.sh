4d2f1320d6a8f1677b008a7286dfb75d79494753
Cory Farmer <mr@cory.farm>
2025-09-02 14:38:40 -0400

    add testing repo, add ko package

--- scripts/alpine.sh
+++ scripts/alpine.sh
@@ -3,8 +3,9 @@
 # Symlink toolbox shims
 ./toolbox-shims.sh
 
+# Enable testing repository
+echo https://dl-cdn.alpinelinux.org/alpine/edge/testing/ >> /etc/apk/repositories
+
 # Update the container and install packages
 apk update && apk upgrade
-grep -v '^#' ./alpine.packages | xargs apk add
-
-rm /etc/machine-id
+grep -v '^#' ./alpine.packages | xargs apk add --no-cache
